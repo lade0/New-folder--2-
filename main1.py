dbe26ec7d549c6d826b21a4a6f43965cc8eaaddd
lade0 <jesufemi33@yahoo.com>
2024-12-13 19:55:37 +0000

    initial

---
+++ main1.py
@@ -0,0 +1,16 @@
+#concept of sets
+my_set={1,9,73,90,90,1}
+print(my_set)
+my_set.pop()
+print(my_set)
+my_set.add(-1)
+print(my_set)
+youre_set={'red','purple','turquoise'}
+our_set={'red','orange','pink'}
+her_set=youre_set.intersection(our_set)
+print(her_set)
+his_set={839,921,7236,0}
+everyones_set={847,7236,0,32}
+print(his_set-everyones_set)
+print(everyones_set-his_set)
+print(his_set^everyones_set)
---
+++ main2.py
@@ -0,0 +1,4 @@
+# concept of arrays
+import array as a_name
+ms_fernandes_array=a_name.array('c',[1,378,12,12,12,2])
+print(ms_fernandes_array)
